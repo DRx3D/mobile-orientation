d31d23a51f832235eded90bc9979c57e72c88043
DrX3D <web3d@realism.com>
2016-03-10 20:55:03 -0800

    Added files via upload
    Added license to each file

--- display.js
+++ display.js
@@ -1,4 +1,33 @@
-// --> Holds information about an X3D scene that can be used for device-controlled display orientation
+/*
+ * Holds information about an X3D scene that can be used for device-controlled display orientation
+ * This is prototype software and may change at any time without warning
+ * An effort will be made to maintain backward compatibility
+
+
+LICENSE
+The MIT License (MIT)
+
+Copyright (c) 2016 DrX3D
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
 function DeviceDisplay (x3dId, transformCompass, transformTilt, transformSpin, name, active, rlt, rrt) {
 	if (x3dId == '') {return null; }
 	this.x3d			= x3dId;
